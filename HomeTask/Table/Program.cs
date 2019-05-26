1c9111c652f466826d4f0907ad3aa6374a74f8f5
Julia <netetskayayulia@gmail.com>
2019-05-27 01:57:32 +0300

    Do all hometask

---
+++ HomeTask/Table/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var number = InputChek();
+                var array = GetTable(number);              
+                Console.WriteLine("Would you like to enter another number? (y/n)");
+                var answer = Console.ReadLine();
+                if (!(answer.Equals("y") || answer.Equals("Y"))) break;
+            }
+        }
+
+        static int InputChek()
+        {            
+            try
+            {
+                Console.WriteLine("Input number:");
+                int num = Int32.Parse(Console.ReadLine());
+                return num;                            
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("The input string had the wrong format.");
+            }
+            return InputChek();
+        }
+      
+        static long[] GetTable(int number)
+        {
+            long[] arr = new long[10];
+            for(int i=0; i<10; i++)
+            {
+                arr[i] = number * (i+1);
+                Console.WriteLine(number + " * " + (i + 1) + " = " + arr[i]);
+            }
+            return arr;
+        }
+    }
+}
---
+++ HomeTask/Week/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var day = GetWeek(InputChek());
+                Console.WriteLine($"You input: {day}");
+                Console.WriteLine("Would you like to enter another number? (y/n)");
+                var answer = Console.ReadLine();
+                if (!(answer.Equals("y") || answer.Equals("Y"))) break;
+            }
+        }
+
+        static int InputChek()
+        {
+            try
+            {
+                Console.WriteLine("Input number:");
+                int number = Int32.Parse(Console.ReadLine());
+                return number;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("The input string had the wrong format.");
+            }
+            return InputChek();
+        }
+
+        static string GetWeek(int number)
+        {
+            switch (number)
+            {
+                case 1:
+                    return "Monday";
+                case 2:
+                    return "Tuesday";
+                case 3:
+                    return "Wednesday";
+                case 4:
+                    return "Thursday";
+                case 5:
+                    return "Friday";
+                case 6:
+                    return "Saturday";
+                case 7:
+                    return "Sunday";
+                default:
+                    return "Error! There are only 7 days of the week!";
+            }
+        }
+    }
+}
